e1980349de094d527de32ccbf0ed64185a2d2e64
Maks Konarski <konarski.maks@gmail.com>
2019-02-09 22:06:51 +0100

    Generic TabComponent edited.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-import {AppRoutingModule} from './app-routing.module';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LayoutModule } from "@angular/cdk/layout";
+import { AppRoutingModule } from "./app-routing.module";
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from "@angular/material";
 
-import { AppComponent } from './app.component';
-import { CauponaNavComponent } from './caupona-nav/caupona-nav.component';
-import { CauponaHeaderComponent } from './caupona-header/caupona-header.component';
-import {DashboardComponent} from './caupona-content/dashboard/dashboard.component';
-import {CauponaContentComponent} from './caupona-content/caupona-content.component';
-import { OrdersComponent } from './caupona-content/orders/orders.component';
-import { WarehouseComponent } from './caupona-content/warehouse/warehouse.component';
-import { FinanceComponent } from './caupona-content/finance/finance.component';
-import { EmployeesComponent } from './caupona-content/employees/employees.component';
-import { ReservationsComponent } from './caupona-content/reservations/reservations.component';
-import { CalendarComponent } from './caupona-content/calendar/calendar.component';
-import { TargetsComponent } from './caupona-content/targets/targets.component';
-import { PartnersComponent } from './caupona-content/partners/partners.component';
-import { SalesComponent } from './caupona-content/sales/sales.component';
-import { CustomersComponent } from './caupona-content/customers/customers.component';
-import { ReportsComponent } from './caupona-content/reports/reports.component';
-import { NotFoundComponent } from './caupona-content/not-found/not-found.component';
-
+import { AppComponent } from "./app.component";
+import { CauponaNavComponent } from "./caupona-nav/caupona-nav.component";
+import { CauponaHeaderComponent } from "./caupona-header/caupona-header.component";
+import { DashboardComponent } from "./caupona-content/dashboard/dashboard.component";
+import { CauponaContentComponent } from "./caupona-content/caupona-content.component";
+import { OrdersComponent } from "./caupona-content/orders/orders.component";
+import { WarehouseComponent } from "./caupona-content/warehouse/warehouse.component";
+import { FinanceComponent } from "./caupona-content/finance/finance.component";
+import { EmployeesComponent } from "./caupona-content/employees/employees.component";
+import { ReservationsComponent } from "./caupona-content/reservations/reservations.component";
+import { CalendarComponent } from "./caupona-content/calendar/calendar.component";
+import { TargetsComponent } from "./caupona-content/targets/targets.component";
+import { PartnersComponent } from "./caupona-content/partners/partners.component";
+import { SalesComponent } from "./caupona-content/sales/sales.component";
+import { CustomersComponent } from "./caupona-content/customers/customers.component";
+import { ReportsComponent } from "./caupona-content/reports/reports.component";
+import { NotFoundComponent } from "./caupona-content/not-found/not-found.component";
+import { TabNavbarComponent } from "./custom-components/tab-navbar/tab-navbar.component";
 
 @NgModule({
   declarations: [
@@ -43,6 +49,7 @@ import { NotFoundComponent } from './caupona-content/not-found/not-found.compone
     CustomersComponent,
     ReportsComponent,
     NotFoundComponent,
+    TabNavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -53,9 +60,9 @@ import { NotFoundComponent } from './caupona-content/not-found/not-found.compone
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule,
+    MatListModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/caupona-content/warehouse/warehouse.component.ts
+++ src/app/caupona-content/warehouse/warehouse.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-warehouse',
-  templateUrl: './warehouse.component.html',
-  styleUrls: ['./warehouse.component.css']
+  selector: "app-warehouse",
+  templateUrl: "./warehouse.component.html",
+  styleUrls: ["./warehouse.component.css"]
 })
 export class WarehouseComponent implements OnInit {
+  titles: string[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.titles = ["title_one", "title_two", "title_three"];
   }
-
 }
