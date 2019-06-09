c25644242549a171fb1dfdb533575b575df0feda
Patryk Śliwiński <patryksliwinski2@gmail.com>
2019-06-09 22:19:33 +0200

    Calendar small fixes & add calendar feature - grayed-out days out of range

--- src/app/check-month-day.directive.ts
+++ src/app/check-month-day.directive.ts
@@ -15,27 +15,38 @@ export class CheckMonthDayDirective implements OnChanges {
   constructor(private el: ElementRef, private renderer2: Renderer2) {}
 
   ngOnChanges() {
-    const { currentMonth } = this.appCheckMonthDay;
-    let { week } = this.appCheckMonthDay;
-    console.log(week);
-    week = week ? week.getDate() : null;
-    console.log(week);
-    console.log(new Date().getDate());
+    const {
+      currentMonth,
+      calendarHeader,
+      actualMode,
+      week
+    } = this.appCheckMonthDay;
+    const weekDate = week ? week.getDate() : null;
+    const weekMonth = week
+      ? week.toLocaleString("en-us", { month: "short" })
+      : null;
     if (
-      week === new Date().getDate() &&
+      weekDate === new Date().getDate() &&
       currentMonth === new Date().toLocaleString("en-us", { month: "short" })
     ) {
       this.renderer2.addClass(this.el.nativeElement, "highligted-day");
     } else {
       this.renderer2.removeClass(this.el.nativeElement, "highligted-day");
     }
-    // console.log(currentMonth);
-    // console.log(week);
-    // console.log(week.toLocaleString("en-us", { month: "short" }));
-    // if (currentMonth !== week.toLocaleString("en-us", { month: "short" })) {
-    //   this.renderer2.addClass(this.el.nativeElement, "unactive-month");
-    // } else {
-    //   this.renderer2.removeClass(this.el.nativeElement, "unactive-month");
-    // }
+    if (
+      currentMonth !== weekMonth &&
+      actualMode === "month" &&
+      !calendarHeader
+    ) {
+      this.renderer2.addClass(
+        this.el.nativeElement.parentNode.parentNode,
+        "unactive-month"
+      );
+    } else {
+      this.renderer2.removeClass(
+        this.el.nativeElement.parentNode.parentNode,
+        "unactive-month"
+      );
+    }
   }
 }
